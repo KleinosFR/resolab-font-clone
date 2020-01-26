daef956f2ef53693fa5e61dc7283053950f5fc61
S LAVAUD <53374849+KleinosFR@users.noreply.github.com>
2020-01-26 15:15:31 +0100

    remove redux dev tool for deploy

--- src/store.js
+++ src/store.js
@@ -6,14 +6,22 @@ import connectedUsersReducer from "./reducers/connectedUsersReducer";
 import socketReducer from "./reducers/socketReducer";
 import thunk from "redux-thunk";
 
-// const store = createStore(
-//     combineReducers({ authReducer, userReducer }),
-//     compose(
-//         applyMiddleware(thunk),
-//         window.__REDUX_DEVTOOLS_EXTENSION__ &&
-//             window.__REDUX_DEVTOOLS_EXTENSION__()
-//     )
-// );
+
+//const store = createStore(
+//  combineReducers({
+//    authReducer,
+//    userReducer,
+//    alertReducer,
+//    connectedUsersReducer,
+//    socketReducer
+//  }),
+//  compose(
+//    applyMiddleware(thunk),
+//    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//  )
+//);
+
+
 
 const store = createStore(
   combineReducers({
@@ -24,8 +32,7 @@ const store = createStore(
     socketReducer
   }),
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
   )
 );
 
